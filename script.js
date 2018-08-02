8dca72bd1c27b1d1325aa1a35a5d9104dc92d46e
misan7 <sergio.mt88@gmail.com>
2018-08-02 12:51:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const master = [
+  { question: "Seguir la misma rutina cada día me relaja", trait: "master" },
+  {
+    question: "Cuando planifico un viaje pienso en todas las opciones posibles",
+    trait: "master"
+  },
+  {
+    question: "Me gustaría que todo el mundo se comportara y fuese serio",
+    trait: "master"
+  },
+  {
+    question: "Disfruto planificando todo al detalle",
+    trait: "master"
+  },
+  {
+    question: "Me gusta seguir las reglas del juego",
+    trait: "master"
+  },
+  { question: "Cuido mucho de lo que tengo", trait: "master" },
+  {
+    question: "Mi familia y amigos me considerarían un tipo tradicional",
+    trait: "master"
+  },
+  {
+    question: "Me considero precavido y cauto",
+    trait: "master"
+  },
+  {
+    question: "Es importante respetar la autoridad",
+    trait: "master"
+  },
+  {
+    question: "Valoro más una amistad leal a una interesante",
+    trait: "master"
+  }
+];
+
+const searcher = [
+  { question: "Me resulta excitante probar cosas nuevas", trait: "searcher" },
+  {
+    question: "A veces, me dejo llevar por el calentón del momento",
+    trait: "searcher"
+  },
+  { question: "Hacer siempre lo mismo es aburrido", trait: "searcher" },
+  { question: "Me considero muy creativo", trait: "searcher" },
+  {
+    question: "Siempre estoy en busca de nuevas experiencias",
+    trait: "searcher"
+  },
+  { question: "¡Soy muy entusiasta!", trait: "searcher" },
+  {
+    question: "No me importa tomar riesgos si con ello cumplo mis objetivos",
+    trait: "searcher"
+  },
+  { question: "Pasar mucho tiempo en casa me aburre", trait: "searcher" },
+  {
+    question: "Mis amigos me describirían como alguien curioso",
+    trait: "searcher"
+  },
+  { question: "Voy a tope de energía todos los días", trait: "searcher" }
+];
