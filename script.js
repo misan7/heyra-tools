0ce0a10f9d5430fdc2076b1dea9ec51aea90cf7b
misan7 <sergio.mt88@gmail.com>
2018-08-02 13:06:43 +0200

    tests

--- script.js
+++ script.js
@@ -59,3 +59,84 @@ const searcher = [
   },
   { question: "Voy a tope de energía todos los días", trait: "searcher" }
 ];
+
+const negotiator = [
+  {
+    question: "Me gusta conocer las necesidades y sentimientos de mis amigos",
+    trait: "negotiator"
+  },
+  {
+    question:
+      "Generalmente, me guío por el corazón cuando tomo decisiones importantes",
+    trait: "negotiator"
+  },
+  {
+    question: "Con frecuencia me sorprendo a mí mismo soñando despierto",
+    trait: "negotiator"
+  },
+  {
+    question: "Puedo cambiar de opinión fácilmente",
+    trait: "negotiator"
+  },
+  {
+    question: "Las pelis emocionales me conmueven tiempo después de verlas",
+    trait: "negotiator"
+  },
+  {
+    question: "A menudo me pierdo en mis pensamientos",
+    trait: "negotiator"
+  },
+  {
+    question:
+      "Siento las emociones más profundamente que la mayoría de la gente",
+    trait: "negotiator"
+  },
+  { question: "Tengo una imaginación muy vívida", trait: "negotiator" },
+  {
+    question:
+      "Al leer, me encanta cuando el escritor describe algo hermoso paralelamente al argumento",
+    trait: "negotiator"
+  },
+  { question: "Me considero empático", trait: "negotiator" }
+];
+
+const analyst = [
+  {
+    question: "Controlo a la perfección mi Smartphone",
+    trait: "analyst"
+  },
+  {
+    question: "Disfruto de una conversación científica",
+    trait: "analyst"
+  },
+  {
+    question: "Me intriga conocer las reglas y patrones del Universo",
+    trait: "analyst"
+  },
+  {
+    question: "Soy más analítico y lógico que la mayoría de personas",
+    trait: "analyst"
+  },
+  {
+    question: "Leo sobre temas intelectuales con regularidad",
+    trait: "analyst"
+  },
+  {
+    question: "Me gusta averiguar cómo funcionan las cosas",
+    trait: "analyst"
+  },
+  {
+    question: "Soy muy poco flexible",
+    trait: "analyst"
+  },
+  {
+    question: "Tengo facilidad para elegir entre distintas alternativas",
+    trait: "analyst"
+  },
+  {
+    question:
+      "Cuando me compro un móvil nuevo quiero saber todas sus características",
+    trait: "analyst"
+  },
+  { question: "Siempre tomo las decisiones por mí mismo", trait: "analyst" }
+];
